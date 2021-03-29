8e08d53aca63eefdd7fc83b0e5512b709f882491
ashleycowell24 <80480233+ashleycowell24@users.noreply.github.com>
2021-03-29 10:07:33 -0400

    update

--- src/app.js
+++ src/app.js
@@ -1,40 +1,15 @@
-let form = document.querySelector("#city-search");
-form.addEventListener("submit", citySearch);
-
-function citySearch(event){
-    event.preventDefault();
-    let city = document.querySelector("#city-search-input");
-
-    let heading = document.querySelector("h1");
-    heading.innerHTML = `${city.value}`;
-
-    let apiKey = "aa9be3525b97615e3cdf28a265e816b5";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}`;
-    axios.get(`${apiUrl}&appid=${apiKey}&units=metric`).then(showTemperature);
+function displayTemperature(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
-function showTemperature(response) {
-  console.log(response.data);
-  let temperature = Math.round(response.data.main.temp);
-  let temperatureElement = document.querySelector("h2")
-  temperatureElement.innerHTML = `${temperature}°C`;
-}
+let apiKey = "aa9be3525b97615e3cdf28a265e816b5";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}&units=metric`;
 
-let now = new Date();
+axios.get(apiUrl).then(displayTemperature);
 
-let h3 = document.querySelector("h3");
 
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday"
-];
-let day = days[now.getDay()];
-
-h3.innerHTML = `${day} ${hours}:${minutes}`;
